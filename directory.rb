e2739f059a5a5be2fcf8ad88841645e1d88f940b
Hodan <hodanahmed0000@gmail.com>
2020-02-10 16:12:10 +0000

    Array update

--- directory.rb
+++ directory.rb
@@ -1,15 +1,20 @@
 #first we print the list of students
 student_count = 8
+
+students = [
+"Ron Weasley" ,
+"Neville Longbottom" ,
+"Dean Thomas" ,
+"Hermione Granger" ,
+"Harry Potter" ,
+"Lavdener Brown" ,
+"Parvati Patil" ,
+"Luna Lovegood"
+]
 puts "The students of Hogwarts School of Witchcraft and Wizardry."
-puts "-------------"
-puts "Ron Weasley"
-puts "Neville Longbottom"
-puts "Dean Thomas"
-puts "Hermione Granger"
-puts "Harry Potter"
-puts "Lavdener Brown"
-puts "Parvati Patil"
-puts "Luna Lovegood"
-# finally, we print out the total number of students
-puts "Overall we have #{student_count} great students!"
-#it’s important that print() doesn’t add new line characters
+puts "-----------------------------"
+students.each do |student|
+  puts student
+end
+
+puts "Overall, we have #{students.count} great students"
