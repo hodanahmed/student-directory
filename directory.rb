2d0784075b8673e56a10990a7e08fb1bba527836
Hodan <hodanahmed0000@gmail.com>
2020-02-11 15:04:06 +0000

    Added Index with HASHES

--- directory.rb
+++ directory.rb
@@ -1,16 +1,21 @@
-#first we print the list of students
-student_count = 8
-
-students = [[
-"Ron Weasley", :feb],
-["Neville Longbottom", :feb],
-["Dean Thomas", :feb],
-["Hermione Granger", :feb],
-["Harry Potter", :feb],
-["Lavdener Brown", :feb],
-["Parvati Patil", :feb],
-["Luna Lovegood", :feb]
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
 
 def print_header
 puts "The students of Hogwarts School of Witchcraft and Wizardry."
@@ -18,8 +23,8 @@ puts "----------------------------------------------------------"
 end
 
 def print(students)
-students.each do |student|
-puts "#{student[0]} (#{student[1]} cohort)"
+students.each_with_index do |student, index|
+puts "#{index}:#{student[:name]} (#{student[:cohort]} cohort)"
 end
 end
 
@@ -27,6 +32,8 @@ def print_footer(students)
 puts "Overall, we have #{students.count} great students"
 end
 
-print_header
+students = input_students
+
 print(students)
+print_header
 print_footer(students)
